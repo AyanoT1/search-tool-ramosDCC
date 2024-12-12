5edee56acc99aceb92f329d5801b159590e990b0
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-12 17:25:17 -0300

    feat: Added a simple header component

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+
+    
+
+  return (
+    <nav className="fixed top-0 p-4 bg-gray-800 w-full flex items-center justify-between">
+      <h1 className="text-white">RamosDCC</h1>
+    </nav>
+  );
+}
