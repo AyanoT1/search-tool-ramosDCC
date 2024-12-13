2eb59a81af6bbe042b94bb3058dd4f85b73f5a9b
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-13 00:29:54 -0300

    style: Changed from fixed to absolute the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 export default function Header() {
 
-    
-
   return (
-    <nav className="fixed top-0 p-4 bg-gray-800 w-full flex items-center justify-between">
+    <nav className="absolute top-0 p-4 bg-gray-800 w-full flex items-center">
       <h1 className="text-white">RamosDCC</h1>
     </nav>
   );
