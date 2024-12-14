185f123401a0e8b773a06065746e5cbe7505d889
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-14 20:16:11 -0300

    feat: added filter box

--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import Mandatory from "./assets/Mandatory.json";
-import Optionals from "./assets/Optionals.json"
+import Optionals from "./assets/Optionals.json";
 import Course from "./Course";
 
 export default function Searchbar() {
   const [currentList, setCourses] = useState([]);
-  const [currentSource, setSource] = useState({...Mandatory, ...Optionals})
-
+  const [currentSource, setSource] = useState({ ...Mandatory, ...Optionals });
 
   function defaultSearcher(e) {
     let value = e.target.value.toLowerCase();
@@ -27,10 +26,11 @@ export default function Searchbar() {
     setCourses(newList);
   }
 
-
   return (
     <>
-      <div name="search-bar"  className="flex items-center flex-col w-screen absolute top-32">
+      <div name="search-bar"
+        className="flex items-center flex-col w-screen absolute top-32"
+      >
         <div className="w-1/2 flex items-center">
           <input
             onChange={defaultSearcher}
@@ -45,7 +45,41 @@ export default function Searchbar() {
         </div>
       </div>
 
-      <div name="course-list"  className="flex items-center flex-col w-screen absolute top-48">
+      <div name="filter-box"
+        className="border border-gray-400 rounded-lg shadow-md p-6 m-10 fixed top-52"
+      >
+        <h2 className="text-2xl">Filters</h2>
+        <hr className="border-gray-400 border" />
+        <div>
+          <h3 className="font-semibold mt-2">Ramos:</h3>
+          <hr className="border-gray-400" />
+          {[
+            [{ ...Mandatory, ...Optionals }, "Todos"],
+            [Mandatory, "Obligatorios"],
+            [Optionals, "Electivos"],
+          ].map((s, index) => {
+            return (
+              <div>
+                <input
+                  type="radio"
+                  name="sourceSelector"
+                  id={index}
+                  value={s[0]}
+                  defaultChecked={index == 0}
+                  className="m-1 "
+                />
+                <label htmlFor={index} className="p-1">
+                  {s[1]}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div name="course-list"
+        className="flex items-center flex-col w-screen absolute top-48"
+      >
         {currentList.map((key) => {
           return (
             <Course
