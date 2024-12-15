9566b3da7b13e9cdffba3eb44b92329b396b356e
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-14 22:25:26 -0300

    feat: implemented source change filter feature (finally... this was HARD)

--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -8,6 +8,11 @@ import Course from "./Course";
 export default function Searchbar() {
   const [currentList, setCourses] = useState([]);
   const [currentSource, setSource] = useState({ ...Mandatory, ...Optionals });
+  const sourceMapping = {
+    Todos: { ...Mandatory, ...Optionals },
+    Obligatorios: Mandatory,
+    Electivos: Optionals,
+  };
 
   function defaultSearcher(e) {
     let value = e.target.value.toLowerCase();
@@ -27,17 +32,35 @@ export default function Searchbar() {
   }
 
   function handleSourceChange(e) {
-    console.log(e.target.value)
+    let newSource = sourceMapping[e.target.value];
+    setSource(newSource);
+    let query = document.getElementById("searchbar-input").value.toLowerCase();
+
+    if (query === "") {
+      setCourses([]);
+      return;
+    }
+
+    const newList = Object.keys(newSource).filter((key) => {
+      return (
+        newSource[key].name.toLowerCase().includes(query) ||
+        newSource[key].desc.toLowerCase().includes(query)
+      );
+    });
+    setCourses(newList);
   }
 
   return (
     <>
-      <div name="search-bar"
+      {/* Searchbar */}
+      <div
+        name="search-bar"
         className="flex items-center flex-col w-screen absolute top-32"
       >
         <div className="w-1/2 flex items-center">
           <input
             onChange={defaultSearcher}
+            id="searchbar-input"
             type="text"
             className="border w-full rounded-full p-2 pl-4 bg-gray-100 border-gray-400 shadow-md"
             placeholder="Busca info de tu ramo aquí"
@@ -49,21 +72,23 @@ export default function Searchbar() {
         </div>
       </div>
 
-      <div name="filter-box"
-        className="border border-gray-400 rounded-lg shadow-md p-6 m-10 fixed top-52"
+      {/* Filter */}
+      <div
+        name="filter-box"
+        className="border border-gray-400 rounded-lg shadow-md p-6 m-10 fixed top-52 z-10"
       >
         <h2 className="text-2xl">Filters</h2>
         <hr className="border-gray-400 border" />
         <div>
           <h3 className="font-semibold mt-2">Ramos:</h3>
           <hr className="border-gray-400" />
-          {["Todos", "Obgligatorios", "Electivos"].map((s, index) => {
+          {["Todos", "Obligatorios", "Electivos"].map((s, index) => {
             return (
               <div key={index}>
                 <input
                   type="radio"
                   name="sourceSelector"
-                  id={"source"+index}
+                  id={"source" + index}
                   value={s}
                   defaultChecked={index == 0}
                   onChange={handleSourceChange}
@@ -78,7 +103,9 @@ export default function Searchbar() {
         </div>
       </div>
 
-      <div name="course-list"
+      {/* Displayer */}
+      <div
+        name="course-list"
         className="flex items-center flex-col w-screen absolute top-48"
       >
         {currentList.map((key) => {
