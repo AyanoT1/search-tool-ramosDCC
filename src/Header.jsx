55bf4b8788ce381a9c8814d653a1e3bd634a3084
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-16 01:29:42 -0300

    updated header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Header() {
-
   return (
-    <nav className="absolute top-0 p-4 bg-gray-800 w-full flex items-center">
-      <h1 className="text-white">RamosDCC</h1>
+    <nav className="absolute top-0 p-4 w-full flex items-center justify-between border-b">
+      <h2 className="text-4xl font-semibold">RamosCC</h2>
+      <a 
+        href="https://github.com/AyanoT1/search-tool-ramosDCC" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="text-4xl mr-2 text-black dark:text-white"
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
     </nav>
   );
 }
