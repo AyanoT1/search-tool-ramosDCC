2899b521fc86f0a27ca9a040a4c45953a3465cab
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-16 02:56:49 -0300

    fix: blank deployed page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/search-tool-ramosDCC"
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
+  server: {
+    open: true,
+  },
+  base: "/search-tool-ramosDCC/",
 })
