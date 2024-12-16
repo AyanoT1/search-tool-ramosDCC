29b7ab37a12e274b2bb0f470a6e9bac3953cd13f
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-16 16:20:16 -0300

    updated about component

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div
       name="about"
-      className="border border-gray-400 rounded-lg shadow-md p-5 pl-7 pr-7 m-10 absolute top-1/4 right-4 w-44 z-10"
+      className="border border-gray-400 rounded-lg shadow-md p-5 pl-7 pr-7 m-10 absolute top-1/4 right-4 w-48 z-10"
     >
       <h2 className="text-md border-b">
         <FontAwesomeIcon icon={faCircleInfo} /> Sobre la app
@@ -17,9 +17,9 @@ export default function About() {
         </a>{" "}
         de la página del CaDCC. <br />
         <br />
-        El tiempo dedicado de 6 es aprox 10h a la semana. <br />
-        <br />
-        La barra busca dentro del nombre y descripción.
+        La barra busca por nombre y descripción del curso. <br /> <br />
+
+        El tiempo dedicado amarillo es aprox su valor en créditos.
       </p>
     </div>
   );
