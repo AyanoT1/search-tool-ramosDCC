7c1fbd41837c06c4621b0408696b51d8c957a9af
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-16 03:06:07 -0300

    fix: anchor ramosCC

--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -184,7 +184,7 @@ export default function Searchbar() {
         className="border border-gray-400 rounded-lg shadow-md p-5 pl-7 pr-7 m-10 absolute top-1/4 right-4 w-44 z-10"
       >
         <h2 className="text-md border-b"><FontAwesomeIcon icon={faCircleInfo} /> Sobre la app</h2>
-        <p className="text-sm">Buscador basado en <a href="https://ramos.cadcc.cl/">RamosCC</a> de la página del CaDCC. <br /><br />
+        <p className="text-sm">Buscador basado en <a className="z-40" href="https://ramos.cadcc.cl/">RamosCC</a> de la página del CaDCC. <br /><br />
         El tiempo dedicado de 6 es aprox el valor en créditos del ramo. <br /><br />
         La barra busca dentro del nombre y descripción.
         </p>
