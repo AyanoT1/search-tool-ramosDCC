141c4a4e8692301026471e88efa1f1a218f7fd3f
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-18 01:18:35 -0300

    feat: Added display component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,6 +3,8 @@ import { useState, createContext } from "react";
 import Mandatory from "../assets/Mandatory.json";
 import Optionals from "../assets/Optionals.json";
 import Searchbar from "./Searchbar";
+import Display from "./Display";
+import { genericSearch } from "../utils/utils";
 
 export const SettingsContext = createContext();
 export const AllSources = { ...Mandatory, ...Optionals };
@@ -11,18 +13,25 @@ const initialSettings = {
   pattern: "",
   source: AllSources,
   sortBy: "code",
-  tags: "Any",
+  isReversed: false,
+  tag: "Any",
 };
 
 function Search() {
   const [settings, setSettings] = useState(initialSettings);
+  const [results, setResults] = useState([]);
 
-  useEffect(() => console.log(settings), [settings])
+  useEffect(() => setResults(genericSearch(settings)), [settings]);
+  useEffect(()=> console.log(results), [results])
 
   return (
-    <SettingsContext.Provider value={{ settings, setSettings }}>
-      <Searchbar />
-    </SettingsContext.Provider>
+    <>
+      <SettingsContext.Provider value={{ settings, setSettings }}>
+        <Searchbar />
+      </SettingsContext.Provider>
+      <Display elements={results}/>
+    </>
+    
   );
 }
 
